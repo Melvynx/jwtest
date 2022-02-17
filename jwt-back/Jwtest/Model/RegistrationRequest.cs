26905ba4c3fad49d8ffb6ad42d73e2a752285b7e
Melvyn M <56388157+Melvynx@users.noreply.github.com>
2022-02-17 14:05:04 +0100

    patch request model

--- jwt-back/Jwtest/Model/RegistrationRequest.cs
+++ jwt-back/Jwtest/Model/RegistrationRequest.cs
@@ -4,12 +4,9 @@ namespace Jwtest;
 
 public class RegistrationRequest
 {
-    [Required]
-    public string Name { get; set; }
+    [Required] public string Name { get; set; }
 
-    [Required]
-    public string Email { get; set; }
+    [Required] public string Email { get; set; }
 
-    [Required]
-    public string Password { get; set; }
+    [Required] public string Password { get; set; }
 }
